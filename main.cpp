98669405e4f8ffccc4db1e530bcf17479e561d9a
ltb427 <1123039786@qq.com>
2020-02-26 20:35:47 +0800

    线程信号量的使用 	1.使用了std::unique_lock<std::mutex>锁。 	2.主线程等待5s，如果子线程没有输入数据则超时。

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+
+void thread_fun(void);
+
+static int g_value = 0;
+static std::condition_variable g_cv;
+static std::mutex g_lk;
 
 int main()
 {
-	std::cout << "HelloCPPUtils" << std::endl;
+	std::thread th(thread_fun);
+	std::unique_lock<std::mutex> lk(g_lk);
+	if (g_cv.wait_for(lk,std::chrono::seconds(5)) == std::cv_status::timeout)
+	{
+		std::cout << "timeout" << std::endl;
+	}
+	else
+	{
+		std::cout << "You Entryed:" << g_value << std::endl;
+	}
+	th.detach();
 	return 0;
+}
+
+void thread_fun(void)
+{
+	std::cin >> g_value;
+	g_cv.notify_one();
 }
