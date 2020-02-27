040f6ae9b3ad9efcb57667f04780a8ee53c94bfd
ltb427 <1123039786@qq.com>
2020-02-27 21:19:36 +0800

    线程启动方式 	单例模式启动(注意类的静态成员要在类外初始化一下)

--- main.cpp
+++ main.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <functional>
 
 class HelloThread 
 {
 public:
-	static void hellothread()
+	void hellothread()
 	{
 		std::cout << "Hello World, I'm A Thread" << std::endl;
 	}
 	static void start()
 	{
-		std::thread th(hellothread);
+		std::thread th(std::bind(&HelloThread::hellothread, &HelloThread::getInsttance()));
 		th.join();
 	}
+	static HelloThread& getInsttance()
+	{
+		if (instance == nullptr)
+		{
+			instance = new HelloThread;
+		}
+		return *instance;
+	}
+private:
+	HelloThread() = default;
+	static HelloThread* instance;
 };
+
+//类的静态成员初始化
+HelloThread* HelloThread::instance = nullptr;
 
 int hello_thread(int a, int b)
 {
