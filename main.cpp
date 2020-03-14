c235221ee1b03fa9e52711fae47b7df655d9dbcb
ltb427 <1123039786@qq.com>
2020-03-14 21:25:05 +0800

    模板函数     1.模板函数的定义     2.inline在模板函数中的使用

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+//函数模板
+template<typename T> inline //inline在模板中的使用方法
+int compare(const T& left, const T& right)
+{
+	return left > right ? 1 : -1;
+}
 
 int main()
 {
-	std::cout << "HelloCPPUtils" << std::endl;
+	//实例化模板
+	cout << compare(1, 0) << endl;
+	vector<int> vec1{ 9,2,3,4 }, vec2{ 4,5,6 };
+	cout << compare(vec1, vec2) << endl;
 	return 0;
 }
