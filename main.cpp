48e61fa5f14a6b098ad930e2a18aae3e7199e6e3
ltb427 <1123039786@qq.com>
2020-03-14 21:33:52 +0800

    编写类型无关的代码：     1.使用函数对象进行运算操作。     2.具体请见分支class。

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <functional>
 
 using namespace std;
 
@@ -7,14 +8,18 @@ using namespace std;
 template<typename T> inline //inline在模板中的使用方法
 int compare(const T& left, const T& right)
 {
-	return left > right ? 1 : -1;
+	if (less<T>()(left, right)) return -1;
+	if (less<T>()(right, left)) return 1;
+	return 0;
 }
+
+
 
 int main()
 {
 	//实例化模板
 	cout << compare(1, 0) << endl;
-	vector<int> vec1{ 9,2,3,4 }, vec2{ 4,5,6 };
+	vector<int> vec1{ 4,5,6,6 }, vec2{ 4,5,6,5 };
 	cout << compare(vec1, vec2) << endl;
 	return 0;
 }
