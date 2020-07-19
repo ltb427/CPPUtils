108a7d9aa354bbac4abde17f6f4885440485ceb7
ltb427 <1123039786@qq.com>
2020-07-19 19:14:06 +0800

    1.用const替代#define 2.用constexpr替代const

--- async.hpp
+++ async.hpp
@@ -21,7 +21,27 @@ auto really_async(F&& f, Args&& ... args)-> std::future<typename std::result_of<
 	return _fut;
 }
 
-#define MSG	"Hello,std::call_once"
+/*
+用const替代#define
+当程序中需要定义一些常量的时候，用const比用#define要好：
+因为#define是属于预处理器的语法，只是简单地进行宏替换，
+而const是C++编译器的语法，能提供类型检查以及调试信息。
+用constexpr替代const
+C++11引入constexpr来区分出编译期常量。由于很多场景需要的
+不仅仅是常量，而是编译时期就能知道值的常量，比如数据的大
+小，模板类的整型参数，设置alignment内存对齐，switch中的标
+签等等。
+const没有从语义上区分出是否为编译期常量，这样可能就会造成
+编译器会为编译期常量开辟全局存储空间，并且不允许类内初始化
+静态非整型变量：
+如下：
+class Foo {
+public :
+	static const double PI = 3.14;        //不允许类内初始化静态非整型变量
+	static constexpr double PI = 3.14;    //OK
+};
+*/
+constexpr auto MSG = "Hello,std::call_once";
 
 void printTask(const char* msg)
 {
