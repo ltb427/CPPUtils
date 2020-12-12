5089b26fd3236c39f451359eca0fb047f85edbae
= <=>
2020-12-12 13:13:08 +0800

    cpp回调方式1：使用c风格的函数指针

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+//使用函数指针
+typedef int(*pfc)(int,int);
+
+int add(int x, int y)
+{
+	return x+y;
+}
+
+int call_fn(pfc ptr, int x, int y)
+{
+	return ptr(x,y);
+}
+
 int main()
 {
-	std::cout << "HelloCPPUtils" << std::endl;
+	std::cout << call_fn(add, 1, 2) << std::endl;
 	return 0;
 }
